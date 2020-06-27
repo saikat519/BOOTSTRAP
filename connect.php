369aba25693448473bf5614650acb82a8354d750
Saikat Mukhopadhyay <49401892+saikat519@users.noreply.github.com>
2020-06-27 12:49:43 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,25 @@
+<?php
+
+$Fname=$_POST["FirstName"];
+$Lname=$_POST["LastName"];
+$Email=$_POST["Email"];
+$Password=$_POST["Password"];
+$State=$_POST["State"];
+$Gender=$_POST["gridRadios"];
+$Comment=$_POST["Comment"];
+
+echo ($Fname);
+echo ("<br>");
+echo ($Lname);
+echo ("<br>");
+echo ($Email);
+echo ("<br>");
+echo ($Password);
+echo ("<br>");
+echo ($State);
+echo ("<br>");
+echo ($Gender);
+
+echo ($Comment);
+
+?>
